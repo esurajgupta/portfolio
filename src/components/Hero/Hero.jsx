7e35ed2bbb61b85007dfb4c2ddef1cd97f8aeb28
SURAJ KUMAR <surajkumar.brijkishorsah@tekafforde.com>
2025-05-18 20:14:07 +0530

    adding skills and change about section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,17 +7,17 @@ export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I'm Ada</h1>
+        <h1 className={styles.title}>Hi, I'm Suraj</h1>
         <p className={styles.description}>
-          I'm a full-stack developer with 5 years of experience using React and
-          NodeJS. Reach out if you'd like to learn more!
+          I'm a Software developer with 2.3+ years of experience using React-Native and
+          React. Reach out if you'd like to learn more!
         </p>
         <a href="mailto:myemail@email.com" className={styles.contactBtn}>
           Contact Me
         </a>
       </div>
       <img
-        src={getImageUrl("hero/heroImage.png")}
+        src={getImageUrl("hero/suraj-hero.png")}
         alt="Hero image of me"
         className={styles.heroImg}
       />
